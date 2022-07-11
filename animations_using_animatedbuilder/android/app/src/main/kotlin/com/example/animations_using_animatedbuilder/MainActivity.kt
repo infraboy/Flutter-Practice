a5cdca8f645072d7cf9f3f19322f7ec8c000684b
infraboy <ayushkumarghosh@gmail.com>
2022-07-11 08:30:08 +0530

    animated builder

---
+++ animations_using_animatedbuilder/android/app/src/main/kotlin/com/example/animations_using_animatedbuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animations_using_animatedbuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animations_using_animatedbuilder/lib/pages/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _curve;
+  late Animation<double> _sizeAnimation;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _curve =
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut);
+    _sizeAnimation = Tween<double>(begin: 0, end: 200).animate(_curve);
+    _colorAnimation =
+        ColorTween(begin: Colors.deepPurple[700], end: Colors.red[700])
+            .animate(_animationController);
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animations"),
+      ),
+      body: Center(
+        child: BoxAnimation(
+          sizeAnimation: _sizeAnimation,
+          colorAnimation: _colorAnimation,
+        ),
+      ),
+    );
+  }
+}
+
+class BoxAnimation extends StatelessWidget {
+  const BoxAnimation(
+      {Key? key, required this.sizeAnimation, required this.colorAnimation})
+      : super(key: key);
+  final Animation<double> sizeAnimation;
+  final Animation<Color?> colorAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: sizeAnimation,
+      builder: (context, child) {
+        return Container(
+          height: sizeAnimation.value,
+          width: sizeAnimation.value,
+          color: colorAnimation.value,
+        );
+      },
+    );
+  }
+}
